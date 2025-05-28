fd8dff5b87fea2b0d51227d9f913cb2e54c7525c
Dimas Febriyanto <dimassfeb@gmail.com>
2025-05-28 16:11:27 +0700

    dotenv

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,9 +1,13 @@
+import { config } from "dotenv";
 import { Pool } from "pg";
 
-export const pool: Pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "sipedis",
-  password: "password",
-  port: 5432,
+// Load .env variables
+config();
+
+export const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
 });
